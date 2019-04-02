6fa0e923bf7b3a9b7f418835653c2fb2cefaa347
Joseph Lin <joseph.lin@aliyun.com>
2019-04-02 21:56:39 +0800

    [develop] add "csdndata" application(Database) for "serv"-django project 1. startapp csdndata 2. change settings.py for add-in csdndata APP 3. change settings.py for allow all hosts 4. develop csdndata/models.py by merging in "CSDNVisualize/CSDNCrawl"    - only UserId part    - makemigrations    - migrate 5. register UserId of csdndata/models.py to administration

--- src/archit_v1/serv/serv/settings.py
+++ src/archit_v1/serv/serv/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = os.environ.get("DEV_CSDN_DATA_SECRET_KEY", "default_secret_key")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*", ]
 
 
 # Application definition
@@ -37,6 +37,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+
+    # develop application
+    "csdndata.apps.CsdndataConfig",
 ]
 
 MIDDLEWARE = [
