d8145c68d02444fd0f30e804a0219b6009c9b56b
Joseph Lin <joseph.lin@aliyun.com>
2019-04-02 23:14:55 +0800

    [develop] add rabbitmq - require $ pip install pika - rabbitmq basic test:     ==== terminal 1 ====     worker/ $ python consumer.py      [*] Waiting for messages......         To exit press CTRL+C, or send "quit" message     ==== terminal 2 ====     serv/ $ python managepy runserver     .....
        now, terminal 2 will display sended message,
        and terminal 1 will print the received message.

--- src/archit_v1/serv/usr/producer.py
+++ src/archit_v1/serv/usr/producer.py
@@ -15,25 +15,60 @@ from functools import partial
 
 from csdndata.models import UserID
 
+import pika
+
+
+HOST_ADDR = 'localhost'
+QUEUE = 'csdndata'
+ROUTING_KEY = 'csdndata'
+
+
+def rabbitmq_init(queue=QUEUE):
+    connection = pika.BlockingConnection(
+        pika.ConnectionParameters(host=HOST_ADDR))
+    channel = connection.channel()
+
+    channel.queue_declare(queue=queue)
+
+    return connection, channel
+
 
 dbg_print = partial(print, file=sys.stderr)
 
 
+class ThreadQuit(Exception):
+    pass
+
+
 def run():
+
+    conn, chnl = rabbitmq_init()
     while True:
         try:
-            loop()
+            loop(conn, chnl)
+        except ThreadQuit:
+            break
         except Exception as err:
             print("{}".format(err), file=sys.stderr)
             sleep(10)
 
+    conn.close()  # it wouldn't run here for now actually!
 
-def loop():
+
+def loop(connection, channel):
     ''''''
 
-    objs = UserID.objects.all()[:3]
-    dbg_print("##{}##".format(ctime()), end="  ", flush=True)
-    [dbg_print("{}".format(obj), end="\t", flush=True) for obj in objs]
-    dbg_print("")
+    userids = UserID.objects.all()[:3]
+    # dbg_print("##{}##".format(ctime()), end="  ", flush=True)
+    # [dbg_print("{}".format(obj), end="\t", flush=True) for obj in objs]
+    # dbg_print("")
+
+    dbg_print("####  {} <> producer  ####".format(ctime()))
+    for userid in userids:
+        channel.basic_publish(exchange='',
+                              routing_key=ROUTING_KEY,
+                              body=userid.user_id)
+
+        dbg_print(" [x] Sent '{}'".format(userid.user_id))
 
     sleep(5)
---
+++ src/archit_v1/worker/consumer.py
@@ -0,0 +1,49 @@
+#!/usr/bin/env python3
+import pika
+
+
+HOST_ADDR = 'localhost'
+QUEUE = 'csdndata'
+ROUTING_KEY = 'csdndata'
+
+
+def rabbitmq_init(queue=QUEUE):
+    connection = pika.BlockingConnection(
+        pika.ConnectionParameters(host=HOST_ADDR))
+    channel = connection.channel()
+
+    channel.queue_declare(queue=queue)
+
+    return connection, channel
+
+
+def callback(ch, method, properties, body):
+    body = body.decode()
+    if body == "quit":
+        ch.basic_cancel(consumer_tag=ROUTING_KEY)
+        ch.stop_consuming()
+    else:
+        print(" [x] Received %r" % body)
+
+
+def worker_be_consumer():
+    connection, channel = rabbitmq_init()
+
+    channel.basic_consume(
+        queue=QUEUE, on_message_callback=callback, auto_ack=True)
+
+    print(' [*] Waiting for messages......')
+    print('\tTo exit press CTRL+C, or send "quit" message')
+    try:
+        channel.start_consuming()
+    except KeyboardInterrupt:
+        channel.basic_cancel(consumer_tag=ROUTING_KEY)
+        channel.stop_consuming()
+
+
+def main():
+    worker_be_consumer()
+
+
+if __name__ == '__main__':
+    main()
