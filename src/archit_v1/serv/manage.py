fb319392b16cf429efa88fa2fdc2c41eee69be78
Joseph Lin <joseph.lin@aliyun.com>
2019-04-02 22:13:17 +0800

    [develop] the running loop-to-death thread solution. the customer thread could use django-models(ORM Database)

--- src/archit_v1/serv/manage.py
+++ src/archit_v1/serv/manage.py
@@ -2,6 +2,28 @@
 import os
 import sys
 
+import server
+
+
+def customization():
+    import threading
+    t = threading.Thread(target=server.run)
+    t.setDaemon(True)
+    t.start()
+    del t
+
+
+def Is_child_processing():
+    import re
+    re_result = re.findall(
+        "{}".format(os.getpid()),
+        os.popen(
+            "ps -ejf | grep {} | grep -v \"grep\"".format(os.getpid())).read()
+    )
+    ret = True if len(re_result) == 1 else False
+    return ret
+
+
 if __name__ == '__main__':
     os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'serv.settings')
     try:
@@ -12,4 +34,8 @@ if __name__ == '__main__':
             "available on your PYTHONPATH environment variable? Did you "
             "forget to activate a virtual environment?"
         ) from exc
+
+    if Is_child_processing():
+        customization()
+
     execute_from_command_line(sys.argv)
